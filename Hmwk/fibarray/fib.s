6641cb172c768f2021bca80f47aa018aa3377aa4
Taylor Marquez <marqueztaylor@CSC11.(none)>
2015-11-11 14:36:42 -0800

    .

--- Hmwk/fibarray/fib.s
+++ Hmwk/fibarray/fib.s
@@ -12,26 +12,28 @@ fibonacci:
 	LDR R1, fibAddr
 	MOV R4, R1          /* Keep a copy of the initial value of r1 in r4 */
 
+	MOV R5, #0
+	STR R5, [R1]
 	MOV R5, #1
-	MOV R5, [R1, +#4]
-	MOV R5, [R1, +#8]
-	MOV R2, #2
-	
+	STR R5, [R1, +#4]
+	STR R5, [R1, +#8]
+	MOV R2, #0
+
 loop:
-	CMP R2, #45
+	CMP R2, #44
 	BEQ check
-	LDR R5, [R1, #-8]
-	MOV R6, [R1, #-4]
+	LDR R5, [R1]
+	LDR R6, [R1, +#4]
 	ADD R7, R5, R6
-	STR R7, [R1]
+	STR R7, [R1, +#8]
 	ADD R1, R1, #4
 	ADD R2, R2, #1
 	B loop
-	
-check:	
+
+check:
 	CMP R0, #45
 	BGT is_gt_45
-	
+
 	CMP R0, #0
 	BGT is_positive
 
@@ -44,7 +46,7 @@ is_gt_45:
 
 
 is_positive:
-	STR R1, [R4, +R0, LSL #2]
+	LDR R1, [R4, +R0, LSL #2]
 
 end:
 	POP {R4, lr}       /* Pop lr and r4 from the stack */
