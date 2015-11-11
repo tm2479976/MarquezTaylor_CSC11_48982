da55198b27f092134de5aa8b809597a68be2f5e1
tm2479976 <tmarquez6@student.rcc.edu>
2015-11-11 14:19:58 -0800

    .

---
+++ Hmwk/fibarray/fib.s
@@ -0,0 +1,53 @@
+.data
+
+.balign
+fib: .skip 46
+
+.text
+
+	.global fibonacci
+
+fibonacci:
+	PUSH {R4, lr}       /* Push r4 and lr onto the stack */
+	LDR R1, fibAddr
+	MOV R4, R1          /* Keep a copy of the initial value of r1 in r4 */
+
+	MOV R5, #1
+	MOV R5, [R1, +#4]
+	MOV R5, [R1, +#8]
+	MOV R2, #2
+	
+loop:
+	CMP R2, #45
+	BEQ check
+	LDR R5, [R1, #-8]
+	MOV R6, [R1, #-4]
+	ADD R7, R5, R6
+	STR R7, [R1]
+	ADD R1, R1, #4
+	ADD R2, R2, #1
+	B loop
+	
+check:	
+	CMP R0, #45
+	BGT is_gt_45
+	
+	CMP R0, #0
+	BGT is_positive
+
+	MOV R0, #0
+	B end
+
+is_gt_45:
+	MOV R1, #0
+	B end
+
+
+is_positive:
+	STR R1, [R4, +R0, LSL #2]
+
+end:
+	POP {R4, lr}       /* Pop lr and r4 from the stack */
+	BX lr              /* Leave factorial */
+
+fibAddr: .word fib
