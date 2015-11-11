317313577ef8879809fcb4f233cc7698f45384dc
Taylor Marquez <marqueztaylor@CSC11.(none)>
2015-11-11 15:13:59 -0800

    .

--- Hmwk/fibarray/fib.s
+++ Hmwk/fibarray/fib.s
@@ -11,26 +11,28 @@ fibonacci:
 	PUSH {R4, lr}       /* Push r4 and lr onto the stack */
 	LDR R1, fibAddr
 	MOV R4, R1          /* Keep a copy of the initial value of r1 in r4 */
-	SUB R8, R0, #2		/* R8=R0-2 */
-	
+
 	/*initialize term 1 and 2*/
 	MOV R5, #1
 	STR R5, [R1, +#4]
 	STR R5, [R1, +#8]
-	MOV R2, #0
-	
+	MOV R2, #2
+
 	CMP R0, #45			/*Branch if R0>45*/
 	BGT is_gt_45
 
-	CMP R0, #0			/*Branch if 0<R0<45*/
+	CMP R0, #2
 	BGT loop
+
+	CMP R0, #0			/*Branch if 0<R0<45*/
+	BGT is_positive
 
 	MOV R1, #0			/*return 0 for invalid input*/
 	B end
 
 loop:
-	CMP R2, R8			/*branch when R2==R8*/
-	BEQ is_positive
+	CMP R2, R0			/*branch when R2==R0*/
+	BGT is_positive
 	LDR R5, [R1]		/*R5=*R1*/
 	LDR R6, [R1, +#4]	/*R5=*(R1+4)*/
 	ADD R7, R5, R6		/*R7=R5+%6*/
@@ -42,7 +44,6 @@ loop:
 is_gt_45:
 	MOV R1, #0			/*return 0 for invalid input*/
 	B end
-
 
 is_positive:
 	LDR R1, [R4, +R0, LSL #2]
